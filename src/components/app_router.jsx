d15ed2fe01592f4a67175dcce3b334b698e24923
FranciscoMunozP <147677946+FranciscoMunozP@users.noreply.github.com>
2023-10-22 21:45:58 -0300

    Update app_router.jsx

--- src/components/app_router.jsx
+++ src/components/app_router.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import HomePage from '../pages/home'
 import InternalPage from '../pages/internal'
 import Calendario from '../pages/calendario'
-import Info from '../pages/info'
+import Info1 from '../pages/info1'
+import Info2 from '../pages/info2'
+import Info3 from '../pages/info3'
+import Info4 from '../pages/info4'
+import Info5 from '../pages/info5'
 
 
 
@@ -15,7 +18,12 @@ const AppRouter = () => {
       <Routes>
         <Route path='/' element={<Calendario />} />
         <Route path='/internal' element={<InternalPage />} />
-        <Route path='/info' element={<Info />} />
+        <Route path='/info1' element={<Info1 />} />
+        <Route path='/info2' element={<Info2 />} />
+        <Route path='/info3' element={<Info3 />} />
+        <Route path='/info4' element={<Info4 />} />
+        <Route path='/info5' element={<Info5 />} />
+        <Route path='/calendario' element={<Calendario />} />
       </Routes>
     </BrowserRouter>
   )
